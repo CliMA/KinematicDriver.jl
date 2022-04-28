7391d8cbc3e8252d310c0743137c24307fddac02
Anna Jaruga <ajaruga@caltech.edu>
2022-04-28 01:51:40 -0700

    mwe of advection with many issues

--- advect.jl
+++ advect.jl
@@ -18,134 +18,143 @@ using OrdinaryDiffEq: ODEProblem, solve, SSPRK33
 import Logging
 import TerminalLoggers
 Logging.global_logger(TerminalLoggers.TerminalLogger())
+
 const FT = Float64
 
-a = FT(0.0)
-b = FT(4pi)
-n = 128
-α = FT(0.1)
+# Instantiate CliMA Parameters
+struct AEPS <: CLIMAParameters.AbstractEarthParameterSet end
+params = AEPS()
 
-w1 = 2.0
-#t1 = 600.0 for future time varying w
+# Set up the computational domain
+z_min = FT(0)
+z_max = FT(2e3)
+n_elem = 128
 
 domain = Domains.IntervalDomain(
-    Geometry.ZPoint{FT}(a),
-    Geometry.ZPoint{FT}(b),
-    boundary_names = (:left, :right),
+    Geometry.ZPoint{FT}(z_min),
+    Geometry.ZPoint{FT}(z_max),
+    boundary_names = (:bottom, :top),
 )
-mesh = Meshes.IntervalMesh(domain, nelems = n)
+mesh = Meshes.IntervalMesh(domain, nelems = n_elem)
 
-cs = Spaces.CenterFiniteDifferenceSpace(mesh)
-fs = Spaces.FaceFiniteDifferenceSpace(cs)
+space = Spaces.CenterFiniteDifferenceSpace(mesh)
+face_space = Spaces.FaceFiniteDifferenceSpace(space)
 
-# VELOCITY: constant right now
-V = w1 .* Geometry.WVector.(ones(FT, fs))
+coord = Fields.coordinate_field(space)
+face_coord = Fields.coordinate_field(face_space)
 
-# Interpolate the water vapor and potTemp profiles
-z_init = [0.0, 740.0, 3260.0]
-qv_init = [0.016, 0.0138, 0.0024]
-theta_init = [279.9, 279.9, 312.66]
+# Define initial condition
+function init_1d_column(::Type{FT}, params, z) where {FT}
+    # physics parameters
+    # R_d::FT = CLIMAParameters.Planet.R_d(params)
 
-# interpolate between the I.C. points
-function interpolate_z(z_init, q_init, z_interp)
-    # find the two spanning z points
-    for i=1:length(z_init)-1
-        if z_interp > z_init[i] && z_interp < z_init[i+1]
-            z1 = z_init[i]
-            i1 = i
-            z2 = z_init[i+1]
-            i2 = i+1
-            break
-        end
-        z1, z2 .= z_init[end]
-        i1, i2 .= length(z_init)
-    end
+    z_0::FT = 0.0
+    z_1::FT = 740.0
+    z_2::FT = 3260.0
+    qv_0::FT = 0.016
+    qv_1::FT = 0.0138
+    qv_2::FT = 0.0024
+    θ_0::FT = 279.9
+    θ_1::FT = 279.9
+    θ_2::FT = 312.66
 
-    # do the linear interpolation
-    q1, q2 = q_init[i1], q_init[i2]
-    if z1 != z2
-        q_interp = q1 + (q2 - q1)/(z2 - z1)*(z_interp - z1)
-    else
-        q_interp = q1
-    end
+    # density, pressure, etc...
 
-    return q_interp
+    # potential temperature
+    θ = z < z_1 ? θ_0 : θ_1 + (θ_2 - θ_1)/(z_2 - z_1) * z
+
+    # water vapour specific humidity (TODO - or is it mixing ratio?)
+    qv = z < z_1 ? qv_0 + (qv_1 - qv_0)/(z_1 - z_0) * z : qv_1 + (qv_2 - qv_1)/(z_2 - z_1) * z
+
+    return(θ = θ, qv = qv)
 end
 
-print(Fields.coordinate_field(cs).z)
-qv_initz = z -> interpolate_z(z_init, qv_init, z) where {z <: FT}
-qv = qv_initz.(Fields.coordinate_field(cs).z)
+Yc = map(coord -> init_1d_column(FT, params, coord.z), coord)
+w = Geometry.WVector.(ones(FT, face_space)) #TODO - should be changing in time
+Y = Fields.FieldVector(Yc = Yc, w = w)
 
-θ_initz = z -> interpolate_z(z_init, theta_init, z) where {z <: FT}
-θ = θ_initz.(Fields.coordinate_field(cs).z)
+# Advection Equation: ∂ϕ/dt = -∂(vΦ)
+function advection_tendency!(dY, Y, _, t)
 
+    Yc = Y.Yc
+    w = Y.w
+    # TODO @. w = Y.w * sin(t) ?
 
-# TRACER(S) that gets advected: theta and qt (up for negotiation)
-θ = sin.(Fields.coordinate_field(cs).z)
-qt = sin.(Fields.coordinate_field(cs).z)
+    dYc = dY.Yc
 
+    θ = Yc.θ
+    qv = Yc.qv
 
-# Solve advection Equation: ∂θ/dt = -∂(vθ)
-# use the advection operator
-function tendency3!(dθ, θ, _, t)
+    dθ = dYc.θ
+    dqv = dYc.qv
 
     fcc = Operators.FluxCorrectionC2C(
-        left = Operators.Extrapolate(),
-        right = Operators.Extrapolate(),
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
     )
     fcf = Operators.FluxCorrectionF2F(
-        left = Operators.Extrapolate(),
-        right = Operators.Extrapolate(),
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
     )
-    A = Operators.AdvectionC2C(
-        left = Operators.SetValue(sin(-t)),
-        right = Operators.Extrapolate(),
+    A_θ = Operators.AdvectionC2C(
+          bottom = Operators.SetValue(279.9),   #TODO - boundary conditions!
+          top = Operators.Extrapolate(),
     )
-    return @. dθ = -A(V, θ)
+
+    A_qv = Operators.AdvectionC2C(
+           bottom = Operators.SetValue(0.016),   #TODO - boundary conditions!
+           top = Operators.Extrapolate(),
+    )
+
+    @. dθ = -A_θ(w, θ)
+    @. dqv = -A_qv(w, qv)
+    return dY
 end
 
+# TODO - add more tendencies
+# function microphysics_tendency!(dθ, θ, _, t)
 
+# Solve the ODE operator
+Δt = 0.001
+problem = ODEProblem(advection_tendency!, Y, (0.0, 10.0))
+solver = solve(
+    problem,
+    SSPRK33(),
+    dt = Δt,
+    saveat = 10 * Δt,
+    progress = true,
+    progress_message = (dt, u, p, t) -> t,
+);
 
-# # Solve the ODE operator
-# Δt = 0.001
-# prob3 = ODEProblem(tendency3!, θ, (0.0, 10.0))
-# sol3 = solve(
-#     prob3,
-#     SSPRK33(),
-#     dt = Δt,
-#     saveat = 10 * Δt,
-#     progress = true,
-#     progress_message = (dt, u, p, t) -> t,
-# );
+ENV["GKSwstype"] = "nul"
+using ClimaCorePlots, Plots
+Plots.GRBackend()
 
-# ENV["GKSwstype"] = "nul"
-# using ClimaCorePlots, Plots
-# Plots.GRBackend()
+dir = "advect"
+path = joinpath(@__DIR__, "output", dir)
+mkpath(path)
 
-# dir = "advect"
-# path = joinpath(@__DIR__, "output", dir)
-# mkpath(path)
+z_centers = parent(Fields.coordinate_field(space))
 
+anim = Plots.@animate for u in solver.u
+    θ = parent(u.Yc.θ)
+    Plots.plot(θ, z_centers)
+end
+Plots.mp4(anim, joinpath(path, "KM_θ.mp4"), fps = 10)
 
-# anim = Plots.@animate for u in sol3.u
-#     Plots.plot(u, xlim = (-1, 1))
-# end
-# Plots.mp4(anim, joinpath(path, "C2C_advect.mp4"), fps = 10)
-# Plots.png(
-#     Plots.plot(sol3.u[end], xlim = (-1, 1)),
-#     joinpath(path, "sol3_advect_end.png"),
-# )
+anim = Plots.@animate for u in solver.u
+    qv = parent(u.Yc.qv)
+    Plots.plot(qv, z_centers)
+end
+Plots.mp4(anim, joinpath(path, "KM_θ.mp4"), fps = 10)
 
-# function linkfig(figpath, alt = "")
-#     # buildkite-agent upload figpath
-#     # link figure in logs if we are running on CI
-#     if get(ENV, "BUILDKITE", "") == "true"
-#         artifact_url = "artifact://$figpath"
-#         print("\033]1338;url='$(artifact_url)';alt='$(alt)'\a\n")
-#     end
-# end
-
-# linkfig(
-#     relpath(joinpath(path, "advect_end.png"), joinpath(@__DIR__, "../..")),
-#     "Advect End Simulation",
-# )
+θ_end = parent(solver.u[end].Yc.θ)
+qv_end = parent(solver.u[end].Yc.qv)
+Plots.png(
+    Plots.plot(θ_end, z_centers),
+    joinpath(path, "KM_θ_end.png"),
+)
+Plots.png(
+    Plots.plot(qv_end, z_centers),
+    joinpath(path, "KM_qv_end.png"),
+)
