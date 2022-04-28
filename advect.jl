13b3488ea1f964cefcc76c09455a64dc8a6be57b
Emily de Jong <edejong@caltech.edu>
2022-04-28 15:39:04 -0700

    Work on variable density & thermo

--- advect.jl
+++ advect.jl
@@ -30,7 +30,7 @@ z_min = FT(0)
 z_max = FT(3e3)
 n_elem = 128
 
-# Updraft momentum flux terms
+# Updraft momentum flux terms and initial conditions
 w1 = 2 # m/s * kg/m3
 t1 = 600 # s
 w_params = (w1 = w1, t1 = t1)
@@ -48,10 +48,44 @@ face_space = Spaces.FaceFiniteDifferenceSpace(space)
 coord = Fields.coordinate_field(space)
 face_coord = Fields.coordinate_field(face_space)
 
+# Thermodynamic states (TODO)
+# - solve for density and pressure as a function of z 
+function rhod(T,p,qv)
+    return Thermodynamics.air_density(params, T, p, qv)
+end
+
+function rhod(θ,qv,rhod0)
+    R_d::FT = CLIMAParameters.Planet.R_d(params)
+    R_v::FT = CLIMAParameters.Planet.R_v(params)
+    c_pv::FT = CLIMAParameters.Planet.cp_v(params)
+    c_pd::FT = CLIMAParameters.Planet.cp_d(params)
+    g::FT = CLIMAParameters.Planet.grav(params)
+    
+    function drho_dz(rhod, _, z)
+        dql_dz=0
+        θ_dry = θ * (1 + qv)^(R_d / c_pd) # TODO: fix this with thermodynamics.jl
+        T = θ_dry * ((rhod * θ_dry) / 1000 * R_d)^(R_d/c_pd / (1 - R_d/c_pd))
+        p = Thermodynamics.air_pressure_given_θ(params, T, rhod, qv) # TODO: should be moist rhod
+        lv = Thermodynamics.latent_heat_vapor(params, T)
+        Rq = R_v / (1/qv + 1) + R_d / (1+qv)
+        cp = c_pv / (1/qv + 1) + c_pd / (1 + qv)
+        rho = Thermodynamics.air_density(params, T, p)
+
+        drhodz = (g / T * rho * (Rq / cp - 1) - p * lv / cp / T^2 * dql_dz) / Rq
+        return drhodz
+    end
+
+    zspan = (z_min, z_max)
+    prob = ODEProblem(drhodz, rhod0, (z_min, z_max))
+    sol = solve(problem);
+
+    return z -> sol.u(z)
+end
+
 # Define initial condition
 function init_1d_column(::Type{FT}, params, z) where {FT}
     # physics parameters
-    # R_d::FT = CLIMAParameters.Planet.R_d(params)
+
 
     z_0::FT = 0.0
     z_1::FT = 740.0
@@ -63,26 +97,26 @@ function init_1d_column(::Type{FT}, params, z) where {FT}
     θ_1::FT = 279.9
     θ_2::FT = 312.66
 
-    # density, pressure, etc...
+    p0::FT = 1002 # hPa
 
     # potential temperature
     θ = z < z_1 ? θ_0 : θ_1 + (θ_2 - θ_1)/(z_2 - z_1) * z
 
     # water vapour specific humidity (TODO - or is it mixing ratio?)
     qv = z < z_1 ? qv_0 + (qv_1 - qv_0)/(z_1 - z_0) * z : qv_1 + (qv_2 - qv_1)/(z_2 - z_1) * z
+    
+    # density, pressure, etc...
+    rhod0 = rhod(θ_0, p0, qv_0)
+    rho = rhod(θ, p0, qv)
 
-    return(θ = θ, qv = qv)
+    return(θ = θ, qv = qv, rho=rho)
 end
 
 Yc = map(coord -> init_1d_column(FT, params, coord.z), coord)
 w = Geometry.WVector.(zeros(FT, face_space))
 Y = Fields.FieldVector(Yc = Yc, w = w)
 
-# Thermodynamic states (TODO)
-# - solve for density and pressure as a function of z 
-function rhod(T,p,q)
-    return air_density(params, T, p, q)
-end
+
 
 # Advection Equation: ∂ϕ/dt = -∂(vΦ)
 function advection_tendency!(dY, Y, _, t, w_params)
