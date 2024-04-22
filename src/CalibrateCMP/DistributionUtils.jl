12181cef05a70a33ff1142f11eae50367fec80f7
Sajjad Azimi <azimi@caltech.edu>
2024-04-21 18:48:54 -0700

    rewrite construct_priors function to fix hanging tests

--- src/CalibrateCMP/DistributionUtils.jl
+++ src/CalibrateCMP/DistributionUtils.jl
@@ -4,15 +4,18 @@ function initial_parameter_ensemble(priors, N_ensemble; rng_seed = 10)
     return initial_ensemble
 end
 
-function construct_priors(params::Dict)
+function construct_priors(
+    params::Dict{String, @NamedTuple{mean::FT, var::FT, lbound::FT, ubound::FT}},
+) where {FT <: Real}
     u_names = collect(keys(params))
     u_values = collect(values(params))
-    u_means = collect([v.mean for v in u_values])
-    u_vars = collect([v.var for v in u_values])
-    u_lbounds = collect([v.lbound for v in u_values])
-    u_ubounds = collect([v.ubound for v in u_values])
+    n_params = length(u_names)
 
     # All vars are approximated as Gaussian in unconstrained space
-    marginal_priors = constrained_gaussian.(u_names, u_means, u_vars, u_lbounds, u_ubounds)
+    marginal_priors = Vector{ParameterDistribution}(undef, n_params)
+    for i in 1:n_params
+        marginal_priors[i] =
+            constrained_gaussian(u_names[i], u_values[i].mean, u_values[i].var, u_values[i].lbound, u_values[i].ubound)
+    end
     return combine_distributions(marginal_priors)
 end
